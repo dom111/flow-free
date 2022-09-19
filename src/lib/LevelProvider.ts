0a550fd548cb92308c602a7cc0f6eb842ed5360f
dom111 <dom111@users.noreply.github.com>
2022-09-19 21:56:00 +0100

    Ensure scaling the grid works for either height or width and have the app container take over the whole screen.

--- src/lib/LevelProvider.ts
+++ src/lib/LevelProvider.ts
@@ -1,9 +1,9 @@
 import Colour from './Colour';
 
 const levels: [number, number, string][] = [
-  // [3, 3, 'a1abc2bc'],
-  // [8, 8, '2b7gfe5d10d1f3b1g2e1c2ca1h3a5h'],
-  // [15, 15, '4o1i9l6a11eb5g5e20j2g26n9f1f9l7h1o7n10j7c6pd3mc4h5p6im3kad23b2k]'],
+  [3, 3, 'a1abc2bc'],
+  [8, 8, '2b7gfe5d10d1f3b1g2e1c2ca1h3a5h'],
+  [15, 15, '4o1i9l6a11eb5g5e20j2g26n9f1f9l7h1o7n10j7c6pd3mc4h5p6im3kad23b2k]'],
   [6, 5, '1d2a4b2b1c2c10da'],
 ];
 
